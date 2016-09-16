6650ed2b4d8378357f8dcdd1a022d798db63149c
Elisha <elisha.java@gmail.com>
2016-09-16 16:08:02 -0500

    added models for database

---
+++ app/src/main/java/com/simpledeveloper/businesssrecon/db/Answer.java
@@ -0,0 +1,56 @@
+package com.simpledeveloper.businesssrecon.db;
+
+import io.realm.RealmObject;
+import io.realm.annotations.Index;
+import io.realm.annotations.PrimaryKey;
+
+public class Answer extends RealmObject {
+
+    @PrimaryKey
+    @Index
+    private long id;
+    private String answer;
+    private long questionId;
+    private String createdAt;
+    private String updatedAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(long questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ app/src/main/java/com/simpledeveloper/businesssrecon/db/Question.java
@@ -0,0 +1,47 @@
+package com.simpledeveloper.businesssrecon.db;
+
+import io.realm.RealmObject;
+import io.realm.annotations.Index;
+import io.realm.annotations.PrimaryKey;
+
+public class Question extends RealmObject {
+
+    @PrimaryKey
+    @Index
+    private long id;
+    private String question;
+    private String createdAt;
+    private String updatedAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
