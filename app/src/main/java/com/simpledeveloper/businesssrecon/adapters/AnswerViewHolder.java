638e538deca8b0810d05fb52d88ad7080dbb9b9f
Elisha <elisha.java@gmail.com>
2016-09-17 06:16:29 -0500

    added ui for each question

--- app/src/main/java/com/simpledeveloper/businesssrecon/adapters/AnswerViewHolder.java
+++ app/src/main/java/com/simpledeveloper/businesssrecon/adapters/AnswerViewHolder.java
@@ -2,11 +2,18 @@ package com.simpledeveloper.businesssrecon.adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.simpledeveloper.businesssrecon.R;
 
 public class AnswerViewHolder extends RecyclerView.ViewHolder {
 
-
+    TextView mQuestion;
+    TextView mAnswer;
     public AnswerViewHolder(View itemView) {
         super(itemView);
+
+        mQuestion = (TextView) itemView.findViewById(R.id.question);
+        mAnswer = (TextView) itemView.findViewById(R.id.answer);
     }
 }
