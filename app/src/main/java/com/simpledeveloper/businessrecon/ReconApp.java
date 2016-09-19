9040a52452435396349d3046fa2373ce22d566df
Elisha <elisha.java@gmail.com>
2016-09-19 05:46:32 -0500

    preparing for release

--- app/src/main/java/com/simpledeveloper/businessrecon/ReconApp.java
+++ app/src/main/java/com/simpledeveloper/businessrecon/ReconApp.java
@@ -2,9 +2,6 @@ package com.simpledeveloper.businessrecon;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
-import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
-
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -19,11 +16,11 @@ public class ReconApp extends Application {
                 .build();
         Realm.setDefaultConfiguration(config);
 
-        Stetho.initialize(
+        /*Stetho.initialize(
                 Stetho.newInitializerBuilder(this)
                         .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
                         .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
-                        .build());
+                        .build());*/
 
     }
 }
